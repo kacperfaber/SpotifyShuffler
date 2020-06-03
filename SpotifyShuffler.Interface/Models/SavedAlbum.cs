41aca400b3ad25e593b0b5bbe0fa91a4bb2e264a
Kacper Faber <kacperf1234@gmail.com>
2020-06-03 11:07:29 +0200

    Added IJsonDeserializer and simple JsonDeserializer implementation.

---
+++ SpotifyShuffler.Interface/Models/SavedAlbum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SpotifyShuffler.Interface
+{
+    public class SavedAlbum
+    {
+        [JsonProperty("added_at")]
+        public DateTime AddedAt { get; set; }
+        
+        [JsonProperty("album")]
+        public SpotifyAlbum Album { get; set; }
+    }
+}
---
+++ SpotifyShuffler.Interface/Types/JsonDeserializer.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using SpotifyShuffler.Interface.Interfaces;
+
+namespace SpotifyShuffler.Interface.Types
+{
+    public class JsonDeserializer : IJsonDeserializer
+    {
+        public T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
