c7a58d79748ca2c9a35d2480f894e704cc6f8aa8
Kacper Faber <kacperf1234@gmail.com>
2020-06-03 11:02:20 +0200

    Added SimpleSpotifyArtist, SimpleSpotifyTrack and SpotifyAlbum

---
+++ SpotifyShuffler.Interface/Models/SimpleSpotifyTrack.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace SpotifyShuffler.Interface
+{
+    public class SimpleSpotifyTrack
+    {
+        [JsonProperty("artists")]
+        public SimpleSpotifyArtist[] Artists { get; set; }
+        
+        [JsonProperty("duration_ms")]
+        public int DurationMs { get; set; }
+        
+        [JsonProperty("href")]
+        public string Href { get; set; }
+        
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+    }
+}
---
+++ SpotifyShuffler.Interface/Models/SpotifyAlbum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SpotifyShuffler.Interface
+{
+    public class SpotifyAlbum
+    {
+        [JsonProperty("album_type")]
+        public string AlbumType { get; set; }
+        
+        [JsonProperty("artists")]
+        public SimpleSpotifyArtist[] Artists { get; set; }
+        
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        [JsonProperty("href")]
+        public string Href { get; set; }
+        
+        [JsonProperty("label")]
+        public string Label { get; set; }
+        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("release_date")]
+        public DateTime ReleaseDate { get; set; }
+        
+        [JsonProperty("tracks")]
+        public Paging<SimpleSpotifyTrack> Tracks { get; set; }
+        
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+    }
+}
