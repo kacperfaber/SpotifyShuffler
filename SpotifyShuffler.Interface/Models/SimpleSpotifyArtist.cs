386a01f3e800787eade028b81b63946894d0c9ea
Kacper Faber <kacperf1234@gmail.com>
2020-06-03 11:02:20 +0200

    Added SimpleSpotifyArtist, SimpleSpotifyTrack and SpotifyAlbum

---
+++ SpotifyShuffler.Interface/Models/SimpleSpotifyArtist.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace SpotifyShuffler.Interface
+{
+    public class SimpleSpotifyArtist
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
+        
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+    }
+}
