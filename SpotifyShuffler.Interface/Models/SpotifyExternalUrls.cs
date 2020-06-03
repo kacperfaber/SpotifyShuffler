7b819b47b8578713b4035aae447c97e7fa7fa32d
Kacper Faber <kacperf1234@gmail.com>
2020-06-03 10:31:14 +0200

    Added projects Interface (api programming interface) and Database. In interface added models.

---
+++ SpotifyShuffler.Interface/Models/SpotifyExternalUrls.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SpotifyShuffler.Interface
+{
+    public class SpotifyExternalUrls
+    {
+        [JsonProperty("spotify")]
+        public string Spotify { get; set; }
+    }
+}
