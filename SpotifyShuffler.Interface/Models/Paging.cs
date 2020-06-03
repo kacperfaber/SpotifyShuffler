58b882d9f591e58912e4d5f4d8ad097ac2f1d256
Kacper Faber <kacperf1234@gmail.com>
2020-06-03 10:48:25 +0200

    Removed models, created generic Paging.

---
+++ SpotifyShuffler.Interface/Models/Paging.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace SpotifyShuffler.Interface
+{
+    public class Paging<TModel>
+    {
+        [JsonProperty("items")]
+        public TModel[] Items { get; set; }
+        
+        [JsonProperty("href")]
+        public string Href { get; set; }
+        
+        [JsonProperty("total")]
+        public int Total { get; set; }
+        
+        [JsonProperty("previous")]
+        public string Previous { get; set; }
+        
+        [JsonProperty("next")]
+        public string Next { get; set; }
+        
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+    }
+}
