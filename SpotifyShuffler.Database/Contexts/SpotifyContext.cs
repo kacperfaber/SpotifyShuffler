8f708c06f8af107ec3a63d727dd7b6584d1842e4
Kacper Faber <kacperf1234@gmail.com>
2020-06-03 16:20:52 +0200

    Created SpotifyContext and 2 models for db.

---
+++ SpotifyShuffler.Database/Contexts/SpotifyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotifyShuffler.Database.Models;
+
+namespace SpotifyShuffler.Database.Contexts
+{
+    public class SpotifyContext : DbContext
+    {
+        public DbSet<Track> Tracks { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+
+        public SpotifyContext(DbContextOptions<SpotifyContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ SpotifyShuffler.Database/Interfaces/ITracksGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SpotifyShuffler.Database.Models;
+using SpotifyShuffler.Interface;
+
+namespace SpotifyShuffler.Database.Interfaces
+{
+    public interface ITracksGenerator
+    {
+        IEnumerable<Track> GenerateTracks(object n = null);
+    }
+}
