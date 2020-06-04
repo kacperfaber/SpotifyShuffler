a1e59d251da12a18a96fd1af26d3c8f7883a3048
Kacper Faber <kacperf1234@gmail.com>
2020-06-04 10:54:43 +0200

    Added ApiRequest, ApiResponse and Authorization. Nothing is lost in programming.

---
+++ SpotifyShuffler.Database/Models/ApiRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Http;
+
+namespace SpotifyShuffler.Database.Models
+{
+    public class ApiRequest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Url { get; set; }
+
+        public HttpMethod HttpMethod { get; set; }
+
+        public string RequestBody { get; set; }
+
+        public DateTime? SendedAt { get; set; }
+
+        [ForeignKey("ResponseId")]
+        public ApiResponse Response { get; set; }
+
+        public Guid ResponseId { get; set; }
+
+        [ForeignKey("AuthorizationId")]
+        public Authorization Authorization { get; set; }
+
+        public Guid AuthorizationId { get; set; }
+
+        [ForeignKey("AuthorizedUserId")]
+        public User AuthorizedUser { get; set; }
+
+        public Guid AuthorizedUserId { get; set; }
+    }
+}
