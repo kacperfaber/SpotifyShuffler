efb7eccf523984fff3a3b22bff551a9d7ecf3e7a
Kacper Faber <kacperf1234@gmail.com>
2020-06-04 10:34:04 +0200

    Added many models like Playlist, PlaylistPrototype or User relationed with SpotifyUser

---
+++ SpotifyShuffler.Database/Contexts/SpotifyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotifyShuffler.Database.Models;
+
+namespace SpotifyShuffler.Database.Contexts
+{
+    public class SpotifyContext : DbContext
+    {
+        public DbSet<Track> Tracks { get; set; }
+
+        public DbSet<PrimaryArtist> PrimaryArtists { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<SpotifyUser> SpotifyUsers { get; set; }
+
+        public DbSet<PlaylistPrototype> PlaylistPrototypes { get; set; }
+
+        public DbSet<Playlist> Playlists { get; set; }
+
+        public SpotifyContext(DbContextOptions<SpotifyContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ SpotifyShuffler.Database/Models/SpotifyUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SpotifyShuffler.Interface.Enums;
+
+namespace SpotifyShuffler.Database.Models
+{
+    public class SpotifyUser
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string SpotifyId { get; set; }
+
+        public string Name { get; set; }
+
+        public bool HasPrivateData { get; set; }
+
+        public string Email { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+        public string Country { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        
+        public Guid UserId { get; set; }
+    }
+}
