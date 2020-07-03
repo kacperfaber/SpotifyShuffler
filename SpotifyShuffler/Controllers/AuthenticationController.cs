45566cec974242e37dddda08648be9958e54fa70
Kacper Faber <kacperf1234@gmail.com>
2020-07-03 13:39:13 +0200

    Removed db-models.

---
+++ SpotifyShuffler/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using AspNet.Security.OAuth.Spotify;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifyShuffler.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet("callback")]
+        public IActionResult SpotifyCallback()
+        {
+            return View("Callback");
+        }
+
+        [HttpGet("authenticate")]
+        public IActionResult Authenticate() => Challenge(SpotifyAuthenticationDefaults.AuthenticationScheme);
+    }
+}
