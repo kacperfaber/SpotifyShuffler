8e0663dd4be5080bb471d7fd0026b56086dcafa7
Kacper Faber <kacperf1234@gmail.com>
2020-07-03 14:39:32 +0200

    Added AddSpotify scheme.

--- SpotifyShuffler/Program.cs
+++ SpotifyShuffler/Program.cs
@@ -16,8 +16,11 @@ namespace SpotifyShuffler
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
+            
     }
 }
