a4ca4389acb0925a80be650721bf60c6f31b8bc5
Kacper Faber <kacperf1234@gmail.com>
2020-07-03 14:23:26 +0200

    Added new models and removed everything different of SpotifyContext.

---
+++ SpotifyShuffler.Database/Models/PlaylistPrototype.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotifyShuffler.Database.Models
+{
+    public class PlaylistPrototype
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey("OwnerId")]
+        public User Owner { get; set; }
+
+        public Guid OwnerId { get; set; }
+
+        public string OriginalPlaylistId { get; set; }
+
+        [ForeignKey("PrototypeDataId")]
+        public PlaylistPrototypeData PrototypeData { get; set; }
+
+        public Guid PrototypeDataId { get; set; }
+    }
+}
---
+++ SpotifyShuffler.Database/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace SpotifyShuffler.Database.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        [InverseProperty("Owner")]
+        public List<PlaylistPrototype> PlaylistPrototypes { get; set; }
+    }
+}
