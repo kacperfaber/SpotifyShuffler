d409ef159b0311f5cc50200aa075995bcd651212
Kacper Faber <kacperf1234@gmail.com>
2020-07-04 13:19:17 +0200

    Registration will be automatic.

--- SpotifyShuffler/Controllers/LoginController.cs
+++ SpotifyShuffler/Controllers/LoginController.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using SpotifyShuffler.Models;
 
 namespace SpotifyShuffler.Controllers
 {
     public class LoginController : Controller
     {
-        [HttpGet("regiter")]
-        public IActionResult Register()
-        {
-            return Content("register account");
-        }
-
         public IActionResult Login()
         {
             throw new NotImplementedException();
