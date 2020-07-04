fe8907a4cf8d21bb475c63f4d9b8eed6ab47b9e7
Kacper Faber <kacperf1234@gmail.com>
2020-07-04 12:02:59 +0200

    Added navigation controller on right side as partial view. He has dropdown with account settings and logout. Sign in did not work.

--- SpotifyShuffler/Models/LayoutModel.cs
+++ SpotifyShuffler/Models/LayoutModel.cs
@@ -1,7 +1,11 @@
-﻿namespace SpotifyShuffler.Models
+﻿using SpotifyShuffler.Database.Models;
+
+namespace SpotifyShuffler.Models
 {
     public class LayoutModel
     {
         public string Title { get; set; }
+
+        public User CurrentUser { get; set; }
     }
 }
