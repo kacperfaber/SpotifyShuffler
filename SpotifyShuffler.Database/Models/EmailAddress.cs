39c5613f29b09cd353ef2e39abc8bdf71239ccad
Kacper Faber <kacperf1234@gmail.com>
2020-07-04 17:36:33 +0200

    Added EmailAddress model. (Validated email (not validated if are same with spotify)).

---
+++ SpotifyShuffler.Database/Models/EmailAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpotifyShuffler.Database.Models
+{
+    public class EmailAddress
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string NormalizedEmail { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? ValidatedAt { get; set; }
+    }
+}
