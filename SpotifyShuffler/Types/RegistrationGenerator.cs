0862b12f39854c70221a6fc750125c86bd6894a9
Kacper Faber <kacperf1234@gmail.com>
2020-07-05 17:18:06 +0200

    Added RegistrationGenerator.

---
+++ SpotifyShuffler/Types/RegistrationGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SpotifyShuffler.Database;
+using SpotifyShuffler.Interfaces;
+
+namespace SpotifyShuffler.Types
+{
+    public class RegistrationGenerator : IRegistrationGenerator
+    {
+        public Registration GenerateRegistration(SpotifyAccount account)
+        {
+            return new Registration
+            {
+                Id = Guid.NewGuid(),
+                SpotifyAccount = account,
+                CreatedAt = DateTime.Now
+            };
+        }
+    }
+}
