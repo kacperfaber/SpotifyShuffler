47895895c72449fd22822436c1f2151e67acd922
Kacper Faber <kacperf1234@gmail.com>
2020-07-05 18:09:21 +0200

    Base logic for RegistrationController. I have to craete user creator use registration.

--- SpotifyShuffler/Migrations/20200705153456_RegistrationMigration.Designer.cs
+++ SpotifyShuffler/Migrations/20200705153456_RegistrationMigration.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpotifyShuffler.Database;
 
 namespace SpotifyShuffler.Migrations
 {
     [DbContext(typeof(SpotifyContext))]
-    partial class SpotifyContextModelSnapshot : ModelSnapshot
+    [Migration("20200705153456_RegistrationMigration")]
+    partial class RegistrationMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SpotifyShuffler/Migrations/20200705153456_RegistrationMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpotifyShuffler.Migrations
+{
+    public partial class RegistrationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    ActivationCode = table.Column<string>(nullable: true),
+                    SendedAt = table.Column<DateTime>(nullable: true),
+                    ActivatedAt = table.Column<DateTime>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    UserCreatedAt = table.Column<DateTime>(nullable: true),
+                    SpotifyAccountId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Registrations_SpotifyAccounts_SpotifyAccountId",
+                        column: x => x.SpotifyAccountId,
+                        principalTable: "SpotifyAccounts",
+                        principalColumn: "SpotifyId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_SpotifyAccountId",
+                table: "Registrations",
+                column: "SpotifyAccountId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registrations");
+        }
+    }
+}
