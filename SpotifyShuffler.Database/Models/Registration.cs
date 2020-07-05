63dc176cdfbb2903d2e03fe2af90a0d099e0848d
Kacper Faber <kacperf1234@gmail.com>
2020-07-05 16:56:01 +0200

    Added Registration.

---
+++ SpotifyShuffler.Database/Models/Registration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpotifyShuffler.Database
+{
+    public class Registration
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string SpotifyId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string ActivationCode { get; set; }
+
+        public DateTime? SendedAt { get; set; }
+
+        public DateTime? ActivatedAt { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime? UserCreatedAt { get; set; }
+    }
+}
