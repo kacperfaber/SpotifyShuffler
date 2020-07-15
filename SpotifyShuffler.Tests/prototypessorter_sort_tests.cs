379e1ba6fa3768f69293f7bc3a34fb8bb94bd507
Kacper Faber <kacperf1234@gmail.com>
2020-07-15 17:19:23 +0200

    Added one test method for sorter.

---
+++ SpotifyShuffler.Tests/prototypessorter_sort_tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SpotifyShuffler.Database;
+using SpotifyShuffler.Types;
+
+namespace SpotifyShuffler.Tests
+{
+    public class prototypessorter_sort_tests
+    {
+
+        void exec(ref List<TrackPrototype> prototypes)
+        {
+            new PrototypesSorter().Sort(ref prototypes);
+        }
+
+        [Test]
+        public void dont_throws_exceptions()
+        {
+            List<TrackPrototype> prototypes = new List<TrackPrototype>();
+            
+            Assert.DoesNotThrow(() => exec(ref prototypes));
+        }
+    }
+}
