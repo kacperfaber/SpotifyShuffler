6533b42e8c816bffb5ac10b79695c52cc2faa24b
Kacper Faber <kacperf1234@gmail.com>
2020-07-16 16:25:50 +0200

    Works but very trivial.

---
+++ SpotifyShuffler.Interface/Payloads/CreatePlaylistPayload.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace SpotifyShuffler.Interface
+{
+    public class CreatePlaylistPayload
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("public")]
+        public bool IsPublic { get; set; }
+        
+        [JsonProperty("collaborative")]
+        public bool Collaborative { get; set; }
+    }
+}
