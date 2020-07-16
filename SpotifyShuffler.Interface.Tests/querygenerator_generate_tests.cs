3f7fe96195e78d15e22504cbd7a9d74010b2d7bc
Kacper Faber <kacperf1234@gmail.com>
2020-07-16 17:59:17 +0200

    Created QueryGenerator - url builder with custom parameters. Ive done tests.

---
+++ SpotifyShuffler.Interface.Tests/querygenerator_generate_tests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace SpotifyShuffler.Interface.Tests
+{
+    public class querygenerator_generate_tests
+    {
+        string exec(string url, params KeyValuePair<string, object>[] pairs)
+        {
+            QueryGenerator queryGenerator = new QueryGenerator(new QueryParameterGenerator());
+            return queryGenerator.Generate(url, new Dictionary<string, object>(pairs));
+        }
+
+        [Test]
+        public void dont_throws_exceptions()
+        {
+            Assert.DoesNotThrow(() => exec("hello", new KeyValuePair<string, object>("x", 0)));
+        }
+
+        [TestCase("localhost:2222", "value_x", 0)]
+        [TestCase("localhost", "name", "kacper")]
+        [TestCase("http://www.google.com/redirect", "x", false)]
+        [TestCase("http://youtube.pl/", "max_", 251)]
+        [TestCase("localhost:25252", "is_public", true)]
+        public void returns_string_are_equals_to_expecteds(string url, string keyName, object value)
+        {
+            string s = exec(url, new KeyValuePair<string, object>(keyName, value));
+
+            Assert.AreEqual(s, $@"{url}?{keyName}={value}");
+        }
+        
+        [TestCase("localhost:2222", "value_x", 0, "helo", "world")]
+        [TestCase("localhost", "name", "kacper", "update", false)]
+        [TestCase("http://www.google.com/redirect", "x", false, "y", 0)]
+        [TestCase("http://youtube.pl/", "max_", 251, "xxx", false)]
+        [TestCase("localhost:25252", "is_public", true, "x", "d")]
+        public void returns_string_are_equals_to_expecteds_when_parameters_count_is_two(string url, string k1, object v1, string k2, object v2)
+        {
+            string s = exec(url, new KeyValuePair<string, object>(k1, v1), new KeyValuePair<string, object>(k2, v2));
+
+            Assert.AreEqual(s, $@"{url}?{k1}={v1}&{k2}={v2}");
+        }
+    }
+}
---
+++ SpotifyShuffler.Interface/Types/QueryGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpotifyShuffler.Interface
+{
+    public class QueryGenerator : IQueryGenerator
+    {
+        public IQueryParameterGenerator QueryParameterGenerator;
+
+        public QueryGenerator(IQueryParameterGenerator queryParameterGenerator)
+        {
+            QueryParameterGenerator = queryParameterGenerator;
+        }
+
+        public string Generate(string url, Dictionary<string, object> queryParameters)
+        {
+            StringBuilder stringBuilder = new StringBuilder(url);
+            List<KeyValuePair<string,object>> list = queryParameters.ToList();
+
+            for (int i = 0; i < queryParameters.Count; i++)
+            {
+                if (i == 0)
+                {
+                    stringBuilder.Append("?");
+                }
+
+                else if (i < queryParameters.Count)
+                {
+                    stringBuilder.Append("&");
+                }
+
+                stringBuilder.Append(QueryParameterGenerator.Generate(list[i]));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
