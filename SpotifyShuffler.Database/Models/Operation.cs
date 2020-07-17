09e47e05c02653c9b275330160bcccc0b1f1fe27
Kacper Faber <kacperf1234@gmail.com>
2020-07-17 13:48:22 +0200

    Added Operation.cs model.

---
+++ SpotifyShuffler.Database/Models/Operation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotifyShuffler.Database
+{
+    public class Operation
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsCanceled { get; set; }
+
+        public DateTime? CanceledAt { get; set; }
+
+        public string SpotifyId { get; set; }
+
+        [InverseProperty("Operation")]
+        public List<PlaylistPrototype> Prototypes { get; set; }
+
+        public bool IsSubmitted { get; set; }
+
+        public DateTime? SubmittedAt { get; set; }
+
+        public string CreatedPlaylistId { get; set; }
+
+        public User User { get; set; }
+    }
+}
