0b92f7a505fa53487cb20b795ba5854eac278ce2
Kacper Faber <kacperf1234@gmail.com>
2020-07-19 13:54:58 +0200

    AddTracks dont working, but CreatePlaylist did.

--- SpotifyShuffler/Payloads/ExecuteOperationPayload.cs
+++ SpotifyShuffler/Payloads/ExecuteOperationPayload.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using SpotifyShuffler.Models;
 
 namespace SpotifyShuffler.Payloads
 {
     public class ExecuteOperationPayload : LayoutModel
     {
+        [FromQuery(Name = "operation_id")]
         public Guid OperationId { get; set; }
+        
+        [FromQuery(Name = "playlist_id")]
 
         public string PlaylistId { get; set; }
     }
