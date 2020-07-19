7f6d9fa67b7bb916c04e0293f6260f014a3babd1
Kacper Faber <kacperf1234@gmail.com>
2020-07-19 12:42:33 +0200

    Added NameYourPlaylist controller.

---
+++ SpotifyShuffler/Models/NameYourPlaylist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SpotifyShuffler.Models
+{
+    public class NameYourPlaylist
+    {
+        public string PlaylistName { get; set; }
+
+        public string PlaylistDescription { get; set; }
+
+        public string PlaylistId { get; set; }
+        public Guid OperationId { get; set; }
+    }
+}
