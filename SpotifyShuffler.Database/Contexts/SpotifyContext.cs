4867ca4a00e8436fd686a70fe660ca67dd42b22f
Kacper Faber <kacperf1234@gmail.com>
2020-07-20 11:01:04 +0200

    Added CompletedPlaylist as system-made spotify playlist. That is related with user.

--- SpotifyShuffler.Database/Contexts/SpotifyContext.cs
+++ SpotifyShuffler.Database/Contexts/SpotifyContext.cs
@@ -6,11 +6,11 @@ namespace SpotifyShuffler.Database
 {
     public class SpotifyContext : IdentityDbContext<User, Role, Guid>
     {
-        public DbSet<Playlist> Playlists { get; set; }
         public DbSet<PlaylistPrototype> PlaylistPrototypes { get; set; }
         public DbSet<SpotifyAccount> SpotifyAccounts { get; set; }
         public DbSet<Registration> Registrations { get; set; }
         public DbSet<Operation> Operations { get; set; }
+        public DbSet<CompletedPlaylist> CompletedPlaylists { get; set; }
 
         public SpotifyContext(DbContextOptions options) : base(options)
         {
---
+++ SpotifyShuffler.Database/Models/CompletedPlaylist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotifyShuffler.Database
+{
+    public class CompletedPlaylist
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string SpotifyId { get; set; }
+
+        public User Owner { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("PlaylistPrototypeId")]
+        public PlaylistPrototype PlaylistPrototype { get; set; }
+
+        public Guid? PlaylistPrototypeId { get; set; }
+    }
+}
