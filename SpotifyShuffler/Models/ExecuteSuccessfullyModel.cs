9b2ffb54c730022ed7bdeccbdc0a43e9349b2cb2
Kacper Faber <kacperf1234@gmail.com>
2020-07-20 11:50:18 +0200

    Added not implemented RaportController and view with model for ExecutedSuccessfully

---
+++ SpotifyShuffler/Models/ExecuteSuccessfullyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SpotifyShuffler.Database;
+using SpotifyShuffler.Interface;
+
+namespace SpotifyShuffler.Models
+{
+    public class ExecuteSuccessfullyModel : LayoutModel
+    {
+        public Operation Operation { get; set; }
+
+        public SpotifyPlaylist SpotifyPlaylist { get; set; }
+
+        public CompletedPlaylist CompletedPlaylist { get; set; }
+    }
+}
