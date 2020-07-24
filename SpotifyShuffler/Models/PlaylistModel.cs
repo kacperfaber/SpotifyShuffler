713f18430fcae9e68800d0ba0d17d5b9ae9b65dc
Kacper Faber <kacperf1234@gmail.com>
2020-07-24 16:54:57 +0200

    Changed home model (PlaylistModel { SimpleSpotifyPlaylist }). Added _PlaylistItem.

---
+++ SpotifyShuffler/Models/PlaylistModel.cs
@@ -0,0 +1,9 @@
+﻿using SpotifyShuffler.Interface;
+
+namespace SpotifyShuffler.Models
+{
+    public class PlaylistModel
+    {
+        public SimpleSpotifyPlaylist SpotifyPlaylist { get; set; }
+    }
+}
