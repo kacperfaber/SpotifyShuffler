927e1080dcaa5f1bcd4e43b2633c38aa9021c21e
Kacper Faber <kacperf1234@gmail.com>
2020-07-24 12:19:23 +0200

    Redirection after 5 second works. readonly inputs on Summary too.

---
+++ SpotifyShuffler/Migrations/Spotify/20200724101727_SpotifyInit.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpotifyShuffler.Migrations.Spotify
+{
+    public partial class SpotifyInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
