5f1b43f044200996d743d64a571b586e4e3d5c69
Kacper Faber <kacperf1234@gmail.com>
2020-07-24 16:35:13 +0200

    Cookies are expiring after 59 minutes. Tested using one minute.

--- SpotifyShuffler.Interface/Types/SpotifyAuthorization.cs
+++ SpotifyShuffler.Interface/Types/SpotifyAuthorization.cs
@@ -1,11 +1,22 @@
-﻿namespace SpotifyShuffler.Interface
+﻿using System.Security.Claims;
+
+namespace SpotifyShuffler.Interface
 {
     public class SpotifyAuthorization : ISpotifyAuthorization
     {
         public string AccessToken { get; set; }
-        
+
         public string AccessTokenType { get; set; } = "Bearer";
-        
+
         public string GetToken() => $"{AccessTokenType} {AccessToken}";
+
+        public static SpotifyAuthorization Create(string accessToken, string tokenType = "Bearer")
+        {
+            return new SpotifyAuthorization
+            {
+                AccessToken = accessToken,
+                AccessTokenType = tokenType
+            };
+        }
     }
 }
