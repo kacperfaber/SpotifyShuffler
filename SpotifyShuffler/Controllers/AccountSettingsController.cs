a718c82fa99f1b5a1113713b6f3c5064af57bc35
Kacper Faber <kacperf1234@gmail.com>
2020-07-24 13:23:59 +0200

    Changed layout - minimally

---
+++ SpotifyShuffler/Controllers/AccountSettingsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SpotifyShuffler.Database;
+using SpotifyShuffler.Models;
+using SpotifyShuffler.Types;
+
+namespace SpotifyShuffler.Controllers
+{
+    [Authorize]
+    public class AccountSettingsController : Controller
+    {
+        public UserManager UserManager;
+
+        public AccountSettingsController(UserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        [HttpGet("account-settings/settings")]
+        public async Task<IActionResult> Settings()
+        {
+            User user = await UserManager.GetUserAsync(HttpContext.User);
+            
+            return View("Settings", new SettingsModel
+            {
+                CurrentUser = user
+            });
+        }
+    }
+}
