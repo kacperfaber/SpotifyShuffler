ebfb9d31ff8c09cd9873a1df9dbe4e7ddbf3f034
Kacper Faber <kacperf1234@gmail.com>
2020-07-25 10:37:47 +0200

    Now im sending to delete each items in collection (duplicates will be sent more than one time.)

---
+++ SpotifyShuffler.Interface/Interfaces/IPlaylistItemsGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SpotifyShuffler.Interface
+{
+    public interface IPlaylistItemsGenerator
+    {
+        IEnumerable<PlaylistItem> Generate(List<SpotifyTrack> tracks);
+    }
+}
