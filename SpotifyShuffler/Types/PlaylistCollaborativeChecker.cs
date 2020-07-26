71aa38c33e455f01603933390ecc20b6dd776d63
Kacper Faber <kacperf1234@gmail.com>
2020-07-26 12:31:44 +0200

    Playlist thats not collaborative and contain to other owner is automatically Kind=CreateNewPlaylist.

--- SpotifyShuffler/Types/PlaylistCollaborativeChecker.cs
+++ SpotifyShuffler/Types/PlaylistCollaborativeChecker.cs
@@ -1,4 +1,5 @@
-﻿using SpotifyShuffler.Database;
+﻿using System.Diagnostics.CodeAnalysis;
+using SpotifyShuffler.Database;
 using SpotifyShuffler.Interface;
 using SpotifyShuffler.Interfaces;
 
@@ -6,9 +7,9 @@ namespace SpotifyShuffler.Types
 {
     public class PlaylistCollaborativeChecker : IPlaylistCollaborativeChecker
     {
-        public bool Check(User user, SpotifyPlaylist playlist)
+        public bool Check(SpotifyAccount account, SpotifyPlaylist playlist)
         {
-            bool isOwner = user.SpotifyAccount.SpotifyId == playlist.Owner.Id;
+            bool isOwner = account.SpotifyId == playlist.Owner.Id;
             bool isCollab = playlist.Collaborative;
 
             return isCollab || isOwner;
