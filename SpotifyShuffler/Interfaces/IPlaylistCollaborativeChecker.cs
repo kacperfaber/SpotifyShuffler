4cc93da1060d1f2bc9f927cf7024ebec35427992
Kacper Faber <kacperf1234@gmail.com>
2020-07-26 12:03:31 +0200

    Added PlaylistCollaborativeChecker. If user cannot to shuffle original playlist, skip dialog.

---
+++ SpotifyShuffler/Interfaces/IPlaylistCollaborativeChecker.cs
@@ -0,0 +1,10 @@
+﻿using SpotifyShuffler.Database;
+using SpotifyShuffler.Interface;
+
+namespace SpotifyShuffler.Interfaces
+{
+    public interface IPlaylistCollaborativeChecker
+    {
+        bool Check(User user, SpotifyPlaylist playlist);
+    }
+}
---
+++ SpotifyShuffler/Types/PlaylistCollaborativeChecker.cs
@@ -0,0 +1,17 @@
+﻿using SpotifyShuffler.Database;
+using SpotifyShuffler.Interface;
+using SpotifyShuffler.Interfaces;
+
+namespace SpotifyShuffler.Types
+{
+    public class PlaylistCollaborativeChecker : IPlaylistCollaborativeChecker
+    {
+        public bool Check(User user, SpotifyPlaylist playlist)
+        {
+            bool isOwner = user.SpotifyAccount.SpotifyId == playlist.Owner.Id;
+            bool isCollab = playlist.Collaborative;
+
+            return isCollab || isOwner;
+        }
+    }
+}
