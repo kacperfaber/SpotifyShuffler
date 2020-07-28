c1bfe167716834f5ff4e1e64a1868be696faddf2
Kacper Faber <kacperf1234@gmail.com>
2020-07-28 12:12:04 +0200

    Left 23 errors in one file - OpController.

--- SpotifyShuffler/Types/Executor.cs
+++ SpotifyShuffler/Types/Executor.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using SpotifyShuffler.Database;
 using SpotifyShuffler.Interface;
@@ -12,23 +14,29 @@ namespace SpotifyShuffler.Types
         public ISpotifyPlaylistCreator PlaylistCreator;
         public SpotifyService SpotifyService;
         public ITracksAdder TracksAdder;
+        public ISpotifyTracksShuffler Shuffler;
 
-        public Executor(ISpotifyPlaylistCreator playlistCreator, ITracksAdder tracksAdder, SpotifyService spotifyService)
+        public Executor(ISpotifyPlaylistCreator playlistCreator, ITracksAdder tracksAdder, SpotifyService spotifyService, ISpotifyTracksShuffler shuffler)
         {
             PlaylistCreator = playlistCreator;
             TracksAdder = tracksAdder;
             SpotifyService = spotifyService;
+            Shuffler = shuffler;
         }
 
         public async Task<ExecuteResult> ExecuteAsync(Operation operation, User user, SpotifyAuthorization authorization)
         {
             PlaylistService playlistService = await SpotifyService.GetAsync<PlaylistService>(authorization);
+            SpotifyPlaylist originalPlaylist = await playlistService.GetPlaylist(operation.OriginalPlaylistId);
 
             if (operation.Kind == OperationKind.CreateNewPlaylist)
             {
-                SpotifyPlaylist playlist = await PlaylistCreator.CreateAsync(operation, user, playlistService);
+                List<SpotifyTrack> tracks = await playlistService.GetAllTracks(operation.OriginalPlaylistId, originalPlaylist.Tracks.Total);
+                IOrderedEnumerable<SpotifyTrack> shuffledTracks = await Shuffler.ShuffleAsync(tracks);
 
-                await TracksAdder.AddAll(playlist, playlistService);
+                SpotifyPlaylist playlist = await PlaylistCreator.CreateAsync(operation, user, playlistService);
+                
+                await TracksAdder.AddAll(shuffledTracks, playlist, playlistService);
 
                 return new ExecuteResult
                 {
@@ -39,14 +47,16 @@ namespace SpotifyShuffler.Types
 
             else if (operation.Kind == OperationKind.UseOriginalPlaylist)
             {
-                SpotifyPlaylist playlist = await playlistService.GetPlaylist(operation.OriginalPlaylistId);
+                List<SpotifyTrack> tracks = await playlistService.GetAllTracks(operation.OriginalPlaylistId, originalPlaylist.Tracks.Total);
 
-                await playlistService.Clear(playlist);
-                await TracksAdder.AddAll(playlist, playlistService);
+                await playlistService.Clear(originalPlaylist);
+                
+                IOrderedEnumerable<SpotifyTrack> shuffledTracks = await Shuffler.ShuffleAsync(tracks);
+                await TracksAdder.AddAll(shuffledTracks, originalPlaylist, playlistService);
 
                 return new ExecuteResult
                 {
-                    Playlist = playlist,
+                    Playlist = originalPlaylist,
                     Operation = operation
                 };
             }
---
+++ SpotifyShuffler/Types/SpotifyTracksShuffler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpotifyShuffler.Interface;
+using SpotifyShuffler.Interfaces;
+
+namespace SpotifyShuffler.Types
+{
+    public class SpotifyTracksShuffler : ISpotifyTracksShuffler
+    {
+        public async Task<IOrderedEnumerable<SpotifyTrack>> ShuffleAsync(List<SpotifyTrack> tracks)
+        {
+            return await Task.Run(() => tracks.OrderBy(x => Guid.NewGuid()));
+        }
+    }
+}
