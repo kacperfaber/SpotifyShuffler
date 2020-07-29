1e02235adad1a43563825342e83b580c787b6fe7
Kacper Faber <kacperf1234@gmail.com>
2020-07-29 15:20:10 +0200

    ConfirmationCodeSender but now is commented with hidden password - Configuration secrets needed.

--- SpotifyShuffler/Types/ConfirmationCodeSender.cs
+++ SpotifyShuffler/Types/ConfirmationCodeSender.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using SpotifyShuffler.Database;
 using SpotifyShuffler.Interfaces;
 
@@ -6,9 +9,36 @@ namespace SpotifyShuffler.Types
 {
     public class ConfirmationCodeSender : IConfirmationCodeSender
     {
+        public IConfiguration Configuration;
+        
+        
+        
         public Task SendAsync(ConfirmationCode confirmationCode)
         {
-            return Task.Run(() => {});
+            // return Task.Run(() =>
+            // {
+            //     SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            //     {
+            //         UseDefaultCredentials = false,
+            //         Credentials = new NetworkCredential("noconkrystian484", ***),
+            //         DeliveryMethod = SmtpDeliveryMethod.Network,
+            //         EnableSsl = true
+            //     };
+            //
+            //     MailAddress senderAddress = new MailAddress();
+            //     MailAddress receiverAddress = new MailAddress(confirmationCode.Email);
+            //
+            //     MailMessage message = new MailMessage(senderAddress, receiverAddress)
+            //     {
+            //         IsBodyHtml = true,
+            //         Body = $"Hello, Your confirmation code is <strong>{confirmationCode.Code}</strong>",
+            //         Subject = "Confirm your email!"
+            //     };
+            //
+            //     client.Send(message);
+            // });
+
+            return Task.Run(() => { });
         }
     }
 }
