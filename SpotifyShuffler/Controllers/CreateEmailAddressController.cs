911373ace52aaa1d08b775db9bf8137797cf8503
Kacper Faber <kacperf1234@gmail.com>
2020-07-29 09:49:28 +0200

    Controller working good. Ive missing ConfirmationCode model in db and EmailAddressManager.

--- SpotifyShuffler/Controllers/CreateEmailAddressController.cs
+++ SpotifyShuffler/Controllers/CreateEmailAddressController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SpotifyShuffler.Database;
+using SpotifyShuffler.Models;
 using SpotifyShuffler.Types;
 
 namespace SpotifyShuffler.Controllers
@@ -10,12 +11,43 @@ namespace SpotifyShuffler.Controllers
     public class CreateEmailAddressController : Controller
     {
         public UserManager UserManager;
-        
-        public async Task<IActionResult> CreateEmail()
+
+        public CreateEmailAddressController(UserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public async Task<IActionResult> CreateEmail(CreateEmailAddressModel model)
+        {
+            User user = await UserManager.GetUserAsync(HttpContext.User);
+            
+            model ??= new CreateEmailAddressModel();
+            model.CurrentUser = user;
+            
+            return View("CreateEmail", model);
+        }
+
+        public async Task<IActionResult> CreateEmailPost(CreateEmailAddressModel model)
         {
             User user = await UserManager.GetUserAsync(HttpContext.User);
 
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (model.IsCodeSent)
+                {
+                    // validate code.
+                }
+
+                else
+                {
+                    model.IsCodeSent = true;
+
+                    // create email.
+                    // create and send ConfirmationCode not related with EmailAddress created above - { code, email, id }.
+                }
+            }
+
+            return await CreateEmail(model);
         }
     }
 }
