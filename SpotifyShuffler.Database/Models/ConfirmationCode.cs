fd5e0940ed545aac3789bcc0cbbdc35291650421
Kacper Faber <kacperf1234@gmail.com>
2020-07-29 09:50:54 +0200

    Added ConfirmationCode model - very simple.

---
+++ SpotifyShuffler.Database/Models/ConfirmationCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpotifyShuffler.Database
+{
+    public class ConfirmationCode
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Code { get; set; }
+    }
+}
